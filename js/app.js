b1e780ce79e9701a5569c24af76400335ecac87f
Your Name <roycse17@gmail.com>
2021-09-13 08:44:42 +0600

    Remove $ from Total Added- products, sum 2 digits decimal number and show in total, 3 column  product show

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ const showProducts = (products) => {
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
       <div>
-    <img class="product-image" src=${image}></img>
+    <img class="product-image" src=${product.image}></img>
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
@@ -32,6 +32,7 @@ const addToCart = (id, price) => {
 
   updateTaxAndCharge();
   document.getElementById("total-Products").innerText = count;
+  updateTotal()
 };
 
 const getInputValue = (id) => {
@@ -42,10 +43,10 @@ const getInputValue = (id) => {
 
 // main price update function
 const updatePrice = (id, value) => {
-  const convertedOldPrice = getInputValue(id);
+  const convertedOldPrice = parseFloat(document.getElementById(id).innerText);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = total.toFixed(2);
 };
 
 // set innerText function
@@ -72,9 +73,10 @@ const updateTaxAndCharge = () => {
 
 //grandTotal update function
 const updateTotal = () => {
-  const grandTotal =
-    getInputValue("price") + getInputValue("delivery-charge") +
-    getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+  const price = parseFloat(document.getElementById('price').innerText);
+  const delivaryCharge = parseFloat(document.getElementById('delivery-charge').innerText);
+  const totalTax = parseFloat(document.getElementById('total-tax').innerText);
+  const grandTotal = price + delivaryCharge + totalTax;
+  document.getElementById("total").innerText = grandTotal.toFixed(2);
 };
 loadProducts();
